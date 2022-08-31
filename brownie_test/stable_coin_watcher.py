e2ab4c83dba757e93cfbc4fa28945e096855c6f5
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-31 11:07:16 -0700

    updated stable_coin_watch.py to load contracts, and then get swap rates for listed pairs. Prints out the rates for each pair

--- brownie_test/stable_coin_watcher.py
+++ brownie_test/stable_coin_watcher.py
@@ -1,11 +1,4 @@
 """
-TODO:
-[SETUP]
-Connect to the network
-Load the user account
-Load the router contract
-Load the token contracts
-
 [DATA STRUCTURES]
 Prepare a data structure for each token
 Prepare a data structure for each unique token pair
@@ -16,15 +9,19 @@ Fetch and store swap rates
 Print interesting results 
 """
 
-# for [SETUP]
+# import needed packages
 import time
 import datetime
 from brownie import *
 
+# load user test_account
 user = accounts.load('test_account')
+
+# connect to avax network
 network.connect('avax-main')
 
-print("Loading COntracts:")
+# load router and stable coin contracts
+print("Loading Contracts:")
 dai_contract = Contract.from_explorer('0xd586e7f844cea2f87f50152665bcbc2c279d8d70')
 mim_contract = Contract.from_explorer('0x130966628846bfd36ff31a822705796e8cb8c18d')
 usdc_contract = Contract.from_explorer('0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664')
@@ -32,6 +29,7 @@ usdt_contract = Contract.from_explorer('0xc7198437980c041c805a1edcba50c1ce5db951
 wavax_contract = Contract.from_explorer('0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7')
 router_contract = Contract.from_explorer('0x60aE616a2155Ee3d9A68541Ba4544862310933d4')
 
+# create dictionaries to store the data for each stablecoin
 dai = {
     "address": dai_contract.address,
     "symbol": dai_contract.symbol(),
@@ -56,6 +54,9 @@ usdt = {
     "decimals": usdt_contract.decimals(),
 }
 
+# create a list of token pairs tuples
+# each pair is listed twice as (a, b) and (b, a)
+# these will be compaired later to try and find arb opportunities
 token_pairs = [
     (dai, mim),
     (mim, dai),
@@ -71,10 +72,16 @@ token_pairs = [
     (mim, usdc),
 ]
 
+# we want to run continuously so we use a while True loop 
 while True:
+    # for each token pair in the token pair list
     for pair in token_pairs:
+        # set token in as the coin being sent in
         token_in = pair[0]
+        # set token out as the coin we want to get out
         token_out = pair[1]
+        # use getAmountsOut to see how much 1 of token in will swap for token out
+        # save just the amount of token out we will get for 1 token in
         qty_out = (
             router_contract.getAmountsOut(
                 1 * (10 ** token_in["decimals"]), 
@@ -85,7 +92,9 @@ while True:
                 ],
             )[-1] / (10 ** token_out["decimals"])
         )
+        # print out the datetime, token being swaped and how much of the other token we will get out
         print(
             f"{datetime.datetime.now().strftime('[%I:%M:%S %p]')} {token_in['symbol']} → {token_out['symbol']}: ({qty_out:.3f})"
         )
+        # wait for 0.1 seconds
         time.sleep(0.1)
